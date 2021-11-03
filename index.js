eacfe75ab9eb99b660c74c523855a72739db435a
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-11-03 20:55:09 +0600

    connected to database

--- index.js
+++ index.js
@@ -6,12 +6,26 @@ require('dotenv').config()
 const app = express();
 const port =8000;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS} @cluster0.0nxkp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0nxkp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+
+async function run(){
+ try{
+    await client.connect();
+    // 
+    console.log('Connected to MongoDB');
+ }
+ finally{
+    // await client.close();
+ }
+}
+
+run().catch(console.dir);
 app.get('/',(req , res)=>{
     res.send('hello world')
 })
