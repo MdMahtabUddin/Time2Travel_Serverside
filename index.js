28a6b5e48d39a7e22d9f82e84431101345bbce99
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-11-06 03:10:44 +0600

    code updated

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express =require('express');
 const { MongoClient } = require('mongodb');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require('dotenv').config()
+const ObjectId = require("mongodb").ObjectId;
+
 
 
 const app = express();
 const port =8000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0nxkp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -17,8 +26,19 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
  try{
     await client.connect();
-    // 
-    console.log('Connected to MongoDB');
+    const database =client.db('time2travel');
+    const serviceCollection = database.collection('services')
+// console.log('database welcome')
+    // post api 
+    app.get('/services', async (req, res) => {
+        const cursor = serviceCollection.find({});
+        const services = await cursor.toArray();
+        res.send(services);
+    });
+
+// add service 
+
+
  }
  finally{
     // await client.close();
